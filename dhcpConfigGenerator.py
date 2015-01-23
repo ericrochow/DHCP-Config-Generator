02c22fc4ac119968b6e9086857c1b61d82db075d
unknown <erochow@IT008.dtnmgt.com>
2015-01-23 11:05:02 -0500

    Write output to a txt file instead of stdout.

--- dhcpConfigGenerator.py
+++ dhcpConfigGenerator.py
@@ -3,14 +3,16 @@ import csv
 octet2 = raw_input("What is the second octet: ")
 csvfile = 'dhcp.csv'
 dhcp = csv.DictReader(open(csvfile, 'rb'), delimiter=',', quotechar='"')
+f = open('dhcpoutput.txt','w')
 for row in dhcp:
-        print "# " + row['name']
-        print "shared-network " + row['name'] + " {"
-        print " subnet 10." + octet2 + "." + row['index'] + ".0 netmask 255.255.255.0 {"
-        print "  range 10." + octet2 + "." + row['index'] + ".2 10." + octet2 + "." + row['index'] + ".254;"
-        print "  authoritative;"
-        print "  option routers 10." + octet2 + "." + row['index'] + ".1;"
-        print "  option subnet-mask 255.255.255.0;"
-        print "  option broadcast-address 10." + octet2 + "." + row['index'] + ".255;"
-        print " }"
-        print "}"
+        f.write("# %s\n" % row['name'])
+        f.write("shared-network %s {\n" % row['name'])
+        f.write(" subnet 10.%s.%s.0 netmask 255.255.255.0\n" % (octet2, row['index']))
+        f.write("  range 10.%s.%s.2 10.%s.%s.245;\n" % (octet2, row['index'], octet2, row['index']))
+        f.write("  authoritative;\n")
+        f.write("  option routers 10.%s.%s.1\n" % (octet2, row['index']))
+        f.write("  option subnet-mask 255.255.255.0;\n")
+        f.write("  option broadcast-address 10.%s.%s.255\n" % (octet2, row['index']))
+        f.write(" }\n")
+        f.write("}\n")
+f.close()
